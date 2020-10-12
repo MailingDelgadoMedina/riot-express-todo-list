6fd0014050da34c1cad759f8272cb642f58a91cd
Mailing <mailing.delgado.scitexas@gmail.com>
2020-10-11 23:11:33 -0400

    updated deploy,js

--- deploy.js
+++ deploy.js
@@ -2,8 +2,10 @@ var cmd = require('node-cmd');
 var path, node_ssh, ssh, fs;
 fs = require('fs');
 path = require('path');
-const {NodeSSH} = require('node-ssh')
-ssh = new NodeSSH();
+//const {NodeSSH} = require('node-ssh')
+node_ssh = require('node-ssh');
+ssh = new node_ssh();
+
 
 
 // the method that starts the deployment process
